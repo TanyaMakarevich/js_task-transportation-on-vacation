268e25745485aacef866b200a4a168ba0b529e34
Tetiana Makarevych <tanyamakarevich81@gmail.com>
2025-03-24 16:31:56 +0200

    add task solution

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -4,7 +4,22 @@
  * @return {number}
  */
 function calculateRentalCost(days) {
-  // write code here
+  const NUMBER_OF_DAYS = days;
+  const BIG_DISCOUNT = 50;
+  const SMALL_DISCOUNT = 20;
+  const LONG_TERM = 7;
+  const SHORT_TERM = 3;
+  const TOTAL_AMOUNT = days * 40;
+
+  if (NUMBER_OF_DAYS >= LONG_TERM) {
+    return TOTAL_AMOUNT - BIG_DISCOUNT;
+  }
+
+  if (NUMBER_OF_DAYS >= SHORT_TERM) {
+    return TOTAL_AMOUNT - SMALL_DISCOUNT;
+  }
+
+  return TOTAL_AMOUNT;
 }
 
 module.exports = calculateRentalCost;
